54c5fd74057f640ac12975f762418be8c82411b7
BengieMine25 <est.bmedinao0201@uml.edu.ni>
2023-04-18 17:29:50 -0500

    Agregado el despliegue para vista en vivo del proyecto en GitHub Pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
-// Cargar los estilos de la aplicacion
+//Cargar los estilos de la aplicacion
 import '/src/css/style.css'
 
-// Activar el plugin de flowbite
+//Activar el plugin de flowbite
 import 'flowbite'
+
+//Cargar la barra de navegacion
+import './barra'
+
+//Cargar el carrusel/slider
+import './slider'
